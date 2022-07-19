69e754b7649403d7ab0085f2fa8e1388dfd3af21
princekatare22 <75197980+princekatare22@users.noreply.github.com>
2022-07-19 22:12:40 +0530

    Time: 85 ms (31.44%), Space: 21.1 MB (5.49%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -6,33 +6,61 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+#include <unordered_map>
+
 class Solution {
 public:
-//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-//         if(headA == NULL || headB == NULL)
-//             return NULL;
-        
-//         unoredered_map <Node *,Node *>
-// };
-
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA == NULL && headB == NULL)
+        if(headA == NULL || headB == NULL)
             return NULL;
+        
+        unordered_map <ListNode * , bool> visited;
         
         ListNode * curr1 = headA;
         ListNode * curr2 = headB;
         
         while(curr1 != NULL)
         {
-            curr2 = headB;
-            while(curr2 != NULL)
+            visited[curr1] = true;
+            curr1 = curr1->next;
+        }
+        
+        while(curr2 != NULL)
+        {
+            if(visited[curr2] == true)
             {
-                if(curr1 == curr2)
-                    return curr1;
-                curr2 = curr2->next;
+                return curr2;
             }
-            curr1= curr1->next;
+            else
+            {
+                visited[curr2] == true;
+            }
+            curr2 = curr2->next;
         }
         return NULL;
     }
+        
 };
+
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         if(headA == NULL && headB == NULL)
+//             return NULL;
+        
+//         ListNode * curr1 = headA;
+//         ListNode * curr2 = headB;
+        
+//         while(curr1 != NULL)
+//         {
+//             curr2 = headB;
+//             while(curr2 != NULL)
+//             {
+//                 if(curr1 == curr2)
+//                     return curr1;
+//                 curr2 = curr2->next;
+//             }
+//             curr1= curr1->next;
+//         }
+//         return NULL;
+//     }
+// };
